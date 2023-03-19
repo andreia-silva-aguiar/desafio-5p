9e709b9e6af3398ff0bfec9a47f6860be85aa0c0
andreia-silva-aguiar <aguiar.silva.andreia@gmail.com>
2023-03-18 19:34:02 -0500

    criacao do app Solicitacao de alteracao de preco

---
+++ solicitacoes_preco175/webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/suite/ui/generic/template/lib/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("deia.desafio5p.solicitacoespreco175.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
